3c8da45cb364c4f3fb9789a3d4eb3b168e7ea66c
Matt Burke <spraints@gmail.com>
2015-12-06 09:36:03 -0500

    List all accounts when I'm logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
+  def index
+    @users =
+      if session[:db_uid].to_s == "10974355"
+        DropboxUser.all
+      else
+        []
+      end
+  end
+
   def reset
     session.destroy
     redirect_to :action => :index
