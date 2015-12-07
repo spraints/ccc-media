c375039e55e5beb3d394a01be225526a960d28b5
Matt Burke <spraints@gmail.com>
2015-12-06 22:42:59 -0500

    find bulletins

--- app/models/tasks/list_bulletins.rb
+++ app/models/tasks/list_bulletins.rb
@@ -3,7 +3,18 @@ module Tasks
     def perform(uid)
       user = DropboxUser.for_uid(uid).first!
       client = DropboxClient.new(user.token)
-      [client.account_info]
+      find_bulletins client, "/Misc"
+    end
+
+    def find_bulletins(client, path, result = [])
+      client.metadata(path).fetch("contents").each do |meta|
+        if meta["is_dir"]
+          find_bulletins(client, meta["path"], result)
+        elsif File.basename(meta["path"]) =~ /^bulletin/
+          result << meta
+        end
+      end
+      result
     end
   end
 end
