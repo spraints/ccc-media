e4be00a542c45ce49f4a90ddc1ead888abe2c084
Matt Burke <spraints@gmail.com>
2018-06-27 18:01:45 -0400

    Get public URLs the new way

--- app/models/tasks/update_public_urls.rb
+++ app/models/tasks/update_public_urls.rb
@@ -4,9 +4,9 @@ module Tasks
       user = DropboxUser.for_uid!(uid)
       return unless user.sync_enabled?
       user.files.expired.each do |file|
-        shared = user.dropbox_client.shares(file.path)
-        file.public_url = shared["url"]
-        file.public_url_expires_at = shared["expires"]
+        shared = user.dropbox_client.create_shared_link_with_settings(file.path)
+        file.public_url = shared.url
+        file.public_url_expires_at = shared.expires
         file.save!
       end
     end
