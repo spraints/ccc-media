077dd3ff5538f5ee91f85a907cfd918adfdb124a
Matt Burke <spraints@gmail.com>
2018-06-27 18:15:42 -0400

    Get the current shared URL if there is one

--- app/models/tasks/update_public_urls.rb
+++ app/models/tasks/update_public_urls.rb
@@ -3,8 +3,10 @@ module Tasks
     def perform(uid)
       user = DropboxUser.for_uid!(uid)
       return unless user.sync_enabled?
+      client = user.dropbox_client
       user.files.expired.each do |file|
-        shared = user.dropbox_client.create_shared_link_with_settings(file.path)
+        shared = client.list_shared_links(path: file.path).links.first
+        shared ||= client.create_shared_link_with_settings(file.path)
         p shared
         file.public_url = shared.url
         file.public_url_expires_at = shared.expires
