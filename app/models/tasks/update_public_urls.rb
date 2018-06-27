52ab0372b14e1456de676841dfbaf4b9b038cfc7
Matt Burke <spraints@gmail.com>
2018-06-27 18:02:09 -0400

    See if I'm missing out on anything

--- app/models/tasks/update_public_urls.rb
+++ app/models/tasks/update_public_urls.rb
@@ -5,6 +5,7 @@ module Tasks
       return unless user.sync_enabled?
       user.files.expired.each do |file|
         shared = user.dropbox_client.create_shared_link_with_settings(file.path)
+        p shared
         file.public_url = shared.url
         file.public_url_expires_at = shared.expires
         file.save!
